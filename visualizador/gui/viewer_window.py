e23ea1953770db54b5283fac86115a75c5bb5925
Everton de Souza <e.rodrigo@outlook.com>
2023-08-25 16:42:14 -0300

    Interface.

--- visualizador/gui/viewer_window.py
+++ visualizador/gui/viewer_window.py
@@ -1,16 +1,14 @@
 from pathlib import Path
-
 from PySide6.QtWidgets import (
     QMainWindow,
     QGraphicsView,
     QGraphicsScene,
     QFileDialog,
-    QGraphicsPixmapItem,
-    QVBoxLayout,
-    QWidget,
+    QGraphicsPixmapItem
 )
 from PySide6.QtGui import QPixmap, QImageReader, QImage, QAction, QPainter
 from PySide6.QtCore import QRectF
+from visualizador.gui.difference_viewer import Ui_DifferenceViewer
 
 
 # Test images
@@ -28,48 +26,54 @@ IMAGE_B = (
 # Disable the file size limit to open images.
 QImageReader.setAllocationLimit(0)
 
-from visualizador.gui.difference_viewer import Ui_DifferenceViewer
 
 class DifferenceViewer(Ui_DifferenceViewer, QMainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        
-        # Open image
+
+        # Open image action
         self.open_action = QAction("Abrir Imagem...", self)
         self.open_action.triggered.connect(self.open_image)
 
-        # Zoom in
+        # Zoom in action
         self.zoom_in_action = QAction("Aumentar Zoom", self)
-        self.zoom_in_action.setShortcut("Ctrl+=")
+        self.zoom_in_action.setShortcut("=")
         self.zoom_in_action.triggered.connect(self.zoom_in)
 
-        # Actual size
-        self.zoom_actual_action = QAction("Zoom 100%", self)
-        self.zoom_actual_action.setShortcut("Ctrl+0")
-        self.zoom_actual_action.triggered.connect(self.zoom_actual)
-
-        # Zoom out
+        # Zoom out action
         self.zoom_out_action = QAction("Diminuir Zoom", self)
-        self.zoom_out_action.setShortcut("Ctrl+-")
+        self.zoom_out_action.setShortcut("-")
         self.zoom_out_action.triggered.connect(self.zoom_out)
-        
-        # self.file_menu = self.menuBar().addMenu("Imagem")
-        # self.view_menu = self.menuBar().addMenu("Zoom")
+
+        # Zoom reset action
+        self.zoom_actual_action = QAction("Resetar Zoom", self)
+        self.zoom_actual_action.setShortcut("0")
+        self.zoom_actual_action.triggered.connect(self.zoom_reset)
+
+        # Menu bar
+        self.file_menu = self.menuBar().addMenu("Imagem")
+        self.file_menu.addAction(self.open_action)
         self.file_menu = self.menuBar()
         self.view_menu = self.menuBar()
-        self.file_menu.addAction(self.open_action)
+        self.view_menu = self.menuBar().addMenu("Zoom")
         self.view_menu.addAction(self.zoom_in_action)
         self.view_menu.addAction(self.zoom_out_action)
         self.view_menu.addAction(self.zoom_actual_action)
-        
+
+        # Buttons
+        self.btn_zoom_in.clicked.connect(self.zoom_in)
+        self.btn_zoom_out.clicked.connect(self.zoom_out)
+        self.btn_zoom_reset.clicked.connect(self.zoom_reset)
+
         self.scene = QGraphicsScene()
         self.view.setScene(self.scene)
         self.view.setDragMode(QGraphicsView.ScrollHandDrag)
         self.view.setRenderHint(QPainter.SmoothPixmapTransform)
         self.view.setViewportUpdateMode(QGraphicsView.FullViewportUpdate)
+        
+        # Just for testing...
         self.open_image()
-
 
     def open_image(self):
         options = QFileDialog.Options()
@@ -84,7 +88,6 @@ class DifferenceViewer(Ui_DifferenceViewer, QMainWindow):
         # )
 
         # Test Image
-        # file_name = r"C:\Users\everton.souza\python\visualizador\visualizador\test_image_a.jpg"
         file_name = IMAGE_A
 
         if file_name:
@@ -112,5 +115,5 @@ class DifferenceViewer(Ui_DifferenceViewer, QMainWindow):
     def zoom_out(self):
         self.view.scale(1 / 1.2, 1 / 1.2)
 
-    def zoom_actual(self):
+    def zoom_reset(self):
         self.view.resetTransform()
