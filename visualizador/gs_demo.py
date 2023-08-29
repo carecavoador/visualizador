4df7137fe79b9df3330874bdb263316c8a112190
Everton de Souza <e.rodrigo@outlook.com>
2023-08-29 16:23:49 -0300

    Refatoring.

--- visualizador/gs_demo.py
+++ visualizador/gs_demo.py
@@ -1,4 +1,16 @@
 from visualizador.gs import gsapi
 
-version_info = gsapi.gsapi_revision()
-print(version_info)
+
+in_filename = r"C:\Users\everton.souza\Desktop\886905 Saco ENV448 Balanced Perro AD Natural Recipe Cerdo 15kg VG.pdf"
+out_filename = r"C:\Users\everton.souza\Desktop\output_low_rgb.jpg"
+params = ['gs',
+          '-dNOPAUSE',
+          '-dBATCH',
+          '-sDEVICE=jpeg',
+        #   '-r300',
+          '-o', out_filename,
+          '-f', in_filename
+]
+
+instance = gsapi.gsapi_new_instance(1)
+gsapi.gsapi_init_with_args(instance, params)
